f2589692babbd23dac4feb6bcce7a330e5e6fa5d
nuomi1 <nuomi1@qq.com>
2019-08-27 23:25:02 +0800

    style: clearly trailing closure

--- ReusableExtension/Classes/FDTemplateLayoutCell+Reusable.swift
+++ ReusableExtension/Classes/FDTemplateLayoutCell+Reusable.swift
@@ -11,67 +11,63 @@ import UITableView_FDTemplateLayoutCell
 public extension UITableView {
     final func fd_heightForCell<T: UITableViewCell>(cellType: T.Type,
                                                     configuration: @escaping (T) -> Void) -> CGFloat where T: Reusable {
-        return fd_heightForCell(withIdentifier: cellType.reuseIdentifier,
-                                configuration: { bareCell in
-                                    guard let cell = bareCell as? T else {
-                                        fatalError(
-                                            "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(cellType.reuseIdentifier)"
-                                                + " - "
-                                                + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
-                                        )
-                                    }
-                                    configuration(cell)
-        })
+        return fd_heightForCell(withIdentifier: cellType.reuseIdentifier) { bareCell in
+            guard let cell = bareCell as? T else {
+                fatalError(
+                    "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(cellType.reuseIdentifier)"
+                        + " - "
+                        + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
+                )
+            }
+            configuration(cell)
+        }
     }
 
     final func fd_heightForCell<T: UITableViewCell>(cellType: T.Type,
                                                     cacheBy indexPath: IndexPath,
                                                     configuration: @escaping (T) -> Void) -> CGFloat where T: Reusable {
         return fd_heightForCell(withIdentifier: cellType.reuseIdentifier,
-                                cacheBy: indexPath,
-                                configuration: { bareCell in
-                                    guard let cell = bareCell as? T else {
-                                        fatalError(
-                                            "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(cellType.reuseIdentifier)"
-                                                + " - "
-                                                + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
-                                        )
-                                    }
-                                    configuration(cell)
-        })
+                                cacheBy: indexPath) { bareCell in
+            guard let cell = bareCell as? T else {
+                fatalError(
+                    "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(cellType.reuseIdentifier)"
+                        + " - "
+                        + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
+                )
+            }
+            configuration(cell)
+        }
     }
 
     final func fd_heightForCell<T: UITableViewCell>(cellType: T.Type,
                                                     cacheByKey key: String,
                                                     configuration: @escaping (T) -> Void) -> CGFloat where T: Reusable {
         return fd_heightForCell(withIdentifier: cellType.reuseIdentifier,
-                                cacheByKey: key as NSString,
-                                configuration: { bareCell in
-                                    guard let cell = bareCell as? T else {
-                                        fatalError(
-                                            "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(cellType.reuseIdentifier)"
-                                                + " - "
-                                                + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
-                                        )
-                                    }
-                                    configuration(cell)
-        })
+                                cacheByKey: key as NSString) { bareCell in
+            guard let cell = bareCell as? T else {
+                fatalError(
+                    "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(cellType.reuseIdentifier)"
+                        + " - "
+                        + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
+                )
+            }
+            configuration(cell)
+        }
     }
 }
 
 public extension UITableView {
     final func fd_heightForHeaderFooterView<T: UITableViewHeaderFooterView>(viewType: T.Type,
                                                                             configuration: @escaping (T) -> Void) -> CGFloat where T: Reusable {
-        return fd_heightForHeaderFooterView(withIdentifier: viewType.reuseIdentifier,
-                                            configuration: { bareView in
-                                                guard let view = bareView as? T else {
-                                                    fatalError(
-                                                        "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(viewType.reuseIdentifier)"
-                                                            + " - "
-                                                            + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
-                                                    )
-                                                }
-                                                configuration(view)
-        })
+        return fd_heightForHeaderFooterView(withIdentifier: viewType.reuseIdentifier) { bareView in
+            guard let view = bareView as? T else {
+                fatalError(
+                    "could not dequeue a \(String(reflecting: UITableViewCell.self)) with identifier \(viewType.reuseIdentifier)"
+                        + " - "
+                        + "must register a nib or a class for the identifier or connect a prototype cell in a storyboard"
+                )
+            }
+            configuration(view)
+        }
     }
 }
